f85dd019c832ade2643e329bbc8d863c58907908
Ramon Silva <ramon.silva109@gmail.com>
2022-11-17 23:39:17 -0300

    adding a config to my db

--- libs/config.php
+++ libs/config.php
@@ -1,4 +1,6 @@
 <?php
 
 
-//config
+define('MYSQL_DATA', 'clube');
+define('MYSQL_USER', 'user_clube');
+define('MYSQL_PASS', 'vu1ADO');
---
+++ public/index.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="shortcut icon" href="assets/icons8-globe-16.png" type="image/png">
+    <title>Clube</title>
+</head>
+<body>
+    
+
+<h1>Oi</h1>
+<script src="assets/bootstrap.bundle.min.js"></script>
+</body>
+</html>
